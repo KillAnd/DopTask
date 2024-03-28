1be59f8944cd14e070c7ba0efd8f8608a96bd84a
kila1 <kila14081996@gmail.com>
2024-03-28 21:41:50 +0300

    Дополнительные задачки. 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        System.out.println("Task 1");
+
+        int one = 47;
+        int two = 13;
+        int as = one/two;
+        int bs = one%two;
+        System.out.println("Результат " + as + " и " + bs + " в остатке");
+
+        //task 2
+        System.out.println("Task 2");
+
+        int a = 54;
+        int b = a % 10;
+        int c = a / 10;
+        System.out.println("Сумма цифр числа " + a + " равна " + (b + c));
+
+        //task 3
+        System.out.println("Task 3");
+
     }
 }
